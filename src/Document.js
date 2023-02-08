2ca7d74228fc944addeb04cd160238ca7b4c22b5
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2023-02-08 21:16:01 +0200

    Modify title content

--- src/Document.js
+++ src/Document.js
@@ -1,15 +1,15 @@
 function Document({ title, content }) {
     const handleScroll = (ev) => {
         if (ev.target.scrollTop + ev.target.clientHeight >= ev.target.scrollHeight) {
-            document.querySelector('button').removeAttribute('disabled');
+            document.querySelector('button').disabled = false;
         }
     };
 
     return (
         <section>
-            <h2 className={title}>{title}</h2>
+            <h1 className={title}>{title}</h1>
             <article className={content} style={{ overflow: 'auto', height: '200px' }} onScroll={handleScroll}>{content}</article>
-            <button disabled>I Agree</button>
+            <button disabled='true'>I Agree</button>
         </section>
     );
 }
